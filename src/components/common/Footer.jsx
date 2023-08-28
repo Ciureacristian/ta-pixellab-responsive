70fc25a91e254f2f0f23f9a7540bfb69652940c0
Ciureacristian <123569444+Ciureacristian@users.noreply.github.com>
2023-08-28 23:44:44 +0300

    1235

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,9 +1,217 @@
 export default function Header() {
-  return <>
-  <section className="header-secondary"></section>
+  return (
+    <>
+       <div className="footer-primary">
+        <div className="footer-primary-inner container">
+          <nav className="footer-nav">
+            <ul>
+              <li>
+                <a href="/" title="Information">
+                  Information
+                </a>
+              </li>
 
-  <section className="header-logo"></section>
+              <li>
+                <a href="/" title="Contact us">
+                  Contact us
+                </a>
+              </li>
 
-  <section className="header-primary"></section>
-  </>
+              <li>
+                <a href="/" title="Track My Order">
+                Track My Order
+                </a>
+              </li>
+
+              <li>
+                <a href="/" title="Shipping">
+                  Shipping
+                </a>
+              </li>
+
+              <li>
+                <a href="/" title="Free Returns*">
+                Free Returns*
+                </a>
+              </li>
+
+              <li>
+                <a href="/" title="My account">
+                My account
+                </a>
+              </li>
+
+              <li>
+                <a href="/" title="SERVICES">
+                SERVICES
+                </a>
+              </li>
+            </ul>
+
+            <ul>
+              <li>
+                <a href="/" title="Ship to store">
+                Ship to store
+                </a>
+              </li>
+
+              <li>
+                <a href="/" title="Gift card">
+                Gift card
+                </a>
+              </li>
+
+              <li>
+                <a href="/" title="Online only">
+                Online only
+                </a>
+              </li>
+
+              <li>
+                <a href="/" title="Tips & tricks">
+                Tips & tricks
+                </a>
+              </li>
+
+              <li>
+                <a href="/" title="Home">
+                Home
+                </a>
+              </li>
+            </ul>
+
+            <ul>
+              <li>
+                <a href="/" title="LOYALTY PROGRAMS">
+                LOYALTY PROGRAMS
+                </a>
+              </li>
+
+              <li>
+                <a href="/" title="Sign up to The Simons">
+                Sign up to The Simons
+                </a>
+              </li>
+
+              <li>
+                <a href="/" title="My account">
+                My account
+                </a>
+              </li>
+
+              <li>
+                <a href="/" title="Conditions">
+                Conditions
+                </a>
+              </li>
+
+              <li>
+                <a href="/" title=" FAQ">
+                FAQ
+                </a>
+              </li>
+            </ul>
+
+            <ul>
+              <li>
+                <a href="/" title="CAREERS">
+                CAREERS
+                </a>
+              </li>
+
+              <li>
+                <a href="/" title="About US">
+                About US
+                </a>
+              </li>
+
+              <li>
+                <a href="/" title="Our Story">
+                Our Story
+                </a>
+              </li>
+
+              <li>
+                <a href="/" title="Art & Culture">
+                Art & Culture
+                </a>
+              </li>
+
+              <li>
+                <a href="/" title="Our private labels">
+                Our private labels
+                </a>
+              </li>
+
+              <li>
+                <a href="/" title="Stores">
+                Stores
+                </a>
+              </li>
+
+            </ul>
+          </nav>
+
+          <section className="footer-contact">
+          <h1><span>Thread Affair </span>Milano</h1>
+
+        <p>
+        Largo Augusto 3, Milano, 20122, Milano, IT
+        </p>
+
+          <a
+            href="tel:+0039 02 760 003 66"
+            title="Call Us"
+          >Tel: 0039 02 760 003 66</a>
+          <a
+            href="mailto:office@thread-affair.com"
+            title="Email us"
+          > office@thread-affair.com</a>
+          </section>
+
+          <section className="footer-connect">
+          <ul>
+            <li>
+              <a
+              href="tel:+0766555555"
+              title="Call us"
+            >
+              <i className="fa-solid fa-square-phone"></i>
+
+              Support
+            </a>
+          </li>
+
+          <li>
+            <a
+              href=""
+              title="Chat"
+            >
+              <i className="fa-solid fa-comments"></i>
+
+              Chat
+            </a>
+          </li>
+
+          <li>
+            <a
+              href="mailto:"
+              title="Email us"
+            >
+              <i className="fa-solid fa-envelope-open-text"></i>
+
+              Email
+            </a>
+          </li>
+            </ul>
+          </section>
+        </div>
+      </div>
+
+      <div className="footer-secondary">
+
+    </div>
+    </>
+  );
+
 }
